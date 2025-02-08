0888eabce21704cb5bf5cb27cd7b98b925e70767
Aosaf098 <aosafibad10@gmail.com>
2025-02-08 23:38:09 +0600

    Stat Responsive

--- src/Components/Stat/Stat.jsx
+++ src/Components/Stat/Stat.jsx
@@ -3,20 +3,20 @@ import React from "react";
 const Stat = () => {
   return (
     <>
-      <div className="w-2/3 mx-auto my-10">
-        <div className="w-1/2 mx-auto">
-          <h1 className="text-center text-4xl font-bold tracking-wider">
+      <div className="lg:w-2/3 mx-auto my-10">
+        <div className="lg:w-1/2 mx-auto px-3">
+          <h1 className="text-center lg:text-4xl text-3xl font-bold tracking-wider">
             Welcome to the PetVet
             <br />
             Vet Clinic & Services
           </h1>
         </div>
-        <div className="w-full mt-14 flex items-center gap-2">
+        <div className="w-full mt-14 flex lg:flex-row flex-col items-center gap-10 lg:gap-2">
           <div className="flex flex-col gap-6 items-center">
             <div className="w-32 mx-auto">
               <h3 className="text-6xl text-center text-green-500 font-bold">12+</h3>
             </div>
-            <p className="text-center w-11/12 text-muted-foreground">
+            <p className="text-center lg:w-11/12 w-3/4 text-muted-foreground">
               Over 12 years of medical experience to secure your pet’s health
             </p>
           </div>
@@ -24,7 +24,7 @@ const Stat = () => {
             <div className="w-32 mx-auto">
               <h3 className="text-6xl text-center text-green-500 font-bold">960</h3>
             </div>
-            <p className="text-center w-11/12 text-muted-foreground">
+            <p className="text-center lg:w-11/12 w-3/4 text-muted-foreground">
               The number of pets successfully treated in our vet clinic
             </p>
           </div>
@@ -32,7 +32,7 @@ const Stat = () => {
             <div className="w-32 mx-auto">
               <h3 className="text-6xl text-center text-green-500 font-bold">100%</h3>
             </div>
-            <p className="text-center w-11/12 text-muted-foreground">
+            <p className="text-center lg:w-11/12 w-3/4 text-muted-foreground">
               Love and professionalism is what we offer along with medical
               services
             </p>
