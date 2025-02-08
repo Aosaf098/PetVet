79e21bb2238c024e3a6d7695992c99bfb229d239
Aosaf098 <aosafibad10@gmail.com>
2025-02-08 22:12:23 +0600

    Hero Responsive

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,8 +4,8 @@ import { Button } from "../ui/button";
 const Hero = () => {
   return (
     <>
-      <div className="w-2/3 mx-auto my-16 flex items-center">
-        <div className="w-1/2 h-[600px] p-4 flex gap-10 flex-col py-28">
+      <div className="lg:w-2/3 mx-auto lg:my-16 my-10 flex lg:flex-row flex-col items-center">
+        <div className="lg:w-1/2 lg:h-[600px] lg:p-4 px-8 flex gap-10 flex-col lg:py-28">
           <h1 className="text-5xl font-bold leading-snug">
             Pawfect Care: Where Happy Pets Meet Expert <span className="bg-green-400 px-3 rounded-sm">Vets!</span>
           </h1>
@@ -15,9 +15,9 @@ const Hero = () => {
             feathered, and scaly friends happy and healthy. Trust our expert
             vets to provide compassionate care—one paw at a time!
           </p>
-          <Button className="w-1/3 p-3">Book an Appointment</Button>
+          <Button className="lg:w-1/3 w-1/2 p-3">Book an Appointment</Button>
         </div>
-        <div className="w-1/2 h-[600px]">
+        <div className="lg:w-1/2 lg:h-[600px]">
           <img src={heroImg} alt="" />
         </div>
       </div>
